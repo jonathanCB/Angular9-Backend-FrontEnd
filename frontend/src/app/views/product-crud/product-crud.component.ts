6ee5fd2e464d8a90f46e6b636aa3d5871300e29a
Jonathan Cardarelli Barbosa <jonas.ca2@gmail.com>
2021-07-20 15:47:06 -0300

    Criação do botão 'Novo Produto' e configuração da rota para o botão linkar com a page. - Tivemos que importar o 'Router no arquivo product-crud.component.ts'

--- frontend/src/app/views/product-crud/product-crud.component.ts
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+//Importanto o router para podermos linkar o botão a tela.
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-product-crud',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCrudComponent implements OnInit {
 
-  constructor() { }
+  //Esperamos receber uma rota via construtor (o próprip Angular fará essa injeção para nós).
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  //Método para redirecionar o botão para a pagina 'create'.
+  navigateToProductCreate(): void {
+    this.router.navigate(['/products/create'])
+  }
 }
