5b277e28bf834284278f13b5b1ddabafa5d43ecc
Jonathan Cardarelli Barbosa <jonas.ca2@gmail.com>
2021-07-27 16:24:29 -0300

    Criação de produto já está funcionando.

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Router } from '@angular/router';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-create',
@@ -8,15 +9,33 @@ import { Router } from '@angular/router';
   styleUrls: ['./product-create.component.css']
 })
 export class ProductCreateComponent implements OnInit {
-
+  /* Criamos um produto somente para testar se estava tudo funcionando. Ao clicarmos em "Salvar" na nossa page, 
+    nosso sistema exibiu a mensagem "Produto criado com sucesso!" e adicionou nosso produto de teste no db.json.
+  product: Product = {
+    name: 'Produto de teste',
+    price: 125.98
+  }
+  */
+  product: Product = {
+    name: '',
+    price: null
+  }
   constructor(private productService: ProductService, private router: Router) { }
 
   ngOnInit(): void {
     
   }
 
+  //O método subscribe serve para que possamos ser notificados quando a resposta chegar.
+  //Quando chegar a resposta, mostraremos a mensagem "Produto criado!".
+  //Logo depois, navegaremos para /products.
   createProduct(): void {
-    this.productService.showMessage('Produto criado!')
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto criado!')
+      this.router.navigate(['/products'])
+    })
+
+    
   }
 
   cancel(): void {
--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,14 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 //Importando snack-bar:
 import { MatSnackBar } from '@angular/material/snack-bar'
+import { Observable } from 'rxjs';
+import { Product } from './product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+ 
+  //Url base dos nossos produtos (comunicação com nosso backend)
+  baseUrl = "http://localhost:3001/products"
 
   //Injetando snack-bar no construtor:
-  constructor(private snackBar: MatSnackBar) { }
+  constructor(private snackBar: MatSnackBar, private http:HttpClient) { }
 
   //Método que utilizará o snack-bar para mostrar uma mensagem na page quando clicar no botão 'Salvar':
   showMessage(msg: string): void {
@@ -17,5 +23,13 @@ export class ProductService {
       horizontalPosition: "right",
       verticalPosition: "top"
     })
+  }
+  /* - Observable é um padrão baseado em eventos 
+  (quando ocorrer um evento, algum trecho de código é chamado de forma reativa).*/
+  //Método para criar um produto através de uma requisição.
+  //Retornaremos um Observable de Product.
+  create(product: Product): Observable<Product> {
+    //Garantimos que nosso método retornará um Observable de Product utilizando <Product>.
+    return this.http.post<Product>(this.baseUrl, product);
   }
 }
