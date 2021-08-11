b7eb45092c7fe25e5b0e643b2554959f5dc3502b
Jonathan Cardarelli Barbosa <jonas.ca2@gmail.com>
2021-08-11 17:36:38 -0300

    Tabela com nossos produtos foi criada!

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -30,6 +30,11 @@ import { ProductRead2Component } from './components/product/product-read2/produc
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort'
+
+import localePt from '@angular/common/locales/pt'
+import { registerLocaleData } from '@angular/common'
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
@@ -64,7 +69,10 @@ import { MatSortModule } from '@angular/material/sort'
     MatSortModule
   ],
   //Se quisermos que um service seja 'visto' fora do nosso módulo, devemos colocá-lo em 'providers'.
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-BR'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
