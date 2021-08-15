ba1b3a14efbba1d7b93959f59a6b224cbc6656c6
Jonathan Cardarelli Barbosa <jonas.ca2@gmail.com>
2021-08-15 19:47:04 -0300

    Método de atualizar um produto está funcionando.

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-update',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductUpdateComponent implements OnInit {
 
-  constructor() { }
+  product!: Product;
 
+  constructor(private productService: ProductService, 
+              private router: Router, 
+              private route: ActivatedRoute
+  ) { }
+
+  /*Para o formulário já vir preenchido definimos o método
+  ngOnInit() desta forma:*/
   ngOnInit(): void {
+    //pegando o parâmetro que veio dentro de um 'mapa de parâmetros:'
+    const id = this.route.snapshot.paramMap.get('id')
+
+    /*para nosso readById(id) aceitar o id, tivemos que fazer
+    uma alteração no arquivo tsconfig.json: "strict": false*/
+    this.productService.readById(id).subscribe(product => {
+      this.product = product
+    })
   }
 
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage('Produto atualizado com sucesso!')
+      this.router.navigate(['/products'])
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
 }
